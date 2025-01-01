4e54e4d2e67609cf82159e22da47878a14b98c85
NicolasMontone <montonenicolas01@gmail.com>
2025-01-01 20:29:02 -0300

    add images

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
-import Image from "./components/image";
+import Image from './components/image'
 
 export default function Home() {
+  const images = [
+    'https://images.unsplash.com/photo-1682687220742-aba13b6e50ba',
+    'https://images.unsplash.com/photo-1682687981674-0927add86f2b',
+    'https://images.unsplash.com/photo-1469474968028-56623f02e42e',
+    'https://images.unsplash.com/photo-1426604966848-d7adac402bff',
+  ]
+
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="w-96 h-96">
-        <Image
-          src="https://images.unsplash.com/photo-1682687220742-aba13b6e50ba"
-          alt="Example image"
-        />
+      <div className="h-screen w-full grid grid-cols-2 grid-rows-2">
+        {images.map((image, index) => (
+          <Image
+            key={image}
+            src={image}
+            alt={`Example image ${index + 1}`}
+            className="w-full h-full object-cover"
+          />
+        ))}
       </div>
     </div>
-  );
+  )
 }
