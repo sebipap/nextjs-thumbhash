325557829e7995267924eec11cdc17b9e3d1b227
Sebastián Papanicolau <sebastian.papanicolau@scale.com>
2025-01-01 23:47:33 -0300

    docs changes

--- app/page.tsx
+++ app/page.tsx
@@ -205,7 +205,9 @@ export default function base64ToUint8Array(base64: string): Uint8Array {
           </section>
 
           <section className="mb-12">
-            <h2 className="text-xl font-bold mb-4">Usage</h2>
+            <h2 className="text-xl font-bold mb-2">Usage</h2>
+            <p className="mb-4">Depending on your needs, you can either:</p>
+            <h3 className="text-lg font-semibold">Save ThumbHash in the DB</h3>
             <div className="mb-6">
               <p className="mb-4 mt-4">
                 1. Generate a thumbhash for your image:
@@ -216,12 +218,13 @@ export default function base64ToUint8Array(base64: string): Uint8Array {
 // During image upload
 async function handleImageUpload(file: File) {
   const arrayBuffer = await file.arrayBuffer();
+  const imageUrl = await uploadImage(file);
   const thumbhash = await generateThumbHash(arrayBuffer);
   
   // Store thumbhash in your database
   await saveToDatabase({
-    imageUrl: 'https://example.com/image.jpg',
-    thumbhash: thumbhash
+    imageUrl,
+    thumbhash
   });
 }`}
               />
@@ -232,13 +235,14 @@ async function handleImageUpload(file: File) {
               <CodeSnippet
                 code={`import Image from '@/components/Image';
 
-export default function MyPage() {
+export default async function Page() {
+  const { imageUrl, thumbhash } = await getFromDatabase();
+
   return (
     <Image
-      src="https://example.com/my-image.jpg"
-      thumbhash="YOUR_STORED_THUMBHASH"
+      src={imageUrl}
+      thumbhash={thumbhash}
       alt="Description"
-      className="w-full h-64"
     />
   );
 }`}
@@ -316,7 +320,7 @@ export default function MyPage() {
           >
             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
           </svg>
-          source ode
+          source code
         </Link>
       </footer>
     </>
